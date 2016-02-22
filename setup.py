7ef393537a3895959fbb986aafba1594fc4caa97
Ivan Tsybulin <tsybulin@crec.mipt.ru>
2016-02-22 17:50:12 +0300

    Fixed module build order

--- setup.py
+++ setup.py
@@ -1,8 +1,22 @@
 from distutils.core import setup, Extension
 
+# http://stackoverflow.com/questions/12491328/python-distutils-not-include-the-swig-generated-module
+
+
+from distutils.command.build import build
+
+class CustomBuild(build):
+    sub_commands = [
+        ('build_ext', build.has_ext_modules),
+        ('build_py', build.has_pure_modules),
+        ('build_clib', build.has_c_libraries),
+        ('build_scripts', build.has_scripts),
+    ]
+
 setup(
     name = 'aniaft',
     version = '0.1',
+    cmdclass = { 'build' : CustomBuild },
     ext_modules = [
         Extension("_aniaft",
             sources=["boundary.cpp", "tree.cpp", "tria.cpp", "mesh.cpp", "aniaft.i"],
